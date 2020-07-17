2e8bacb35f5cd1ceefdcb9a4a0b9a9ba2e0a71a4
morisho ongala <mlghub@gmail.com>
2020-07-17 11:01:55 +0200

    decomposition done!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopComponentComponent } from './components/top-component/top-component.component';
+import { BtmComponentComponent } from './components/btm-component/btm-component.component';
+import { LeftComponentComponent } from './components/btm-component/left-component/left-component.component';
+import { RightComponentComponent } from './components/btm-component/right-component/right-component.component';
+import { TodoComponentComponent } from './components/shared/todo-component/todo-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponentComponent,
+    BtmComponentComponent,
+    LeftComponentComponent,
+    RightComponentComponent,
+    TodoComponentComponent,
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/btm-component/btm-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btm-component',
+  templateUrl: './btm-component.component.html',
+  styleUrls: ['./btm-component.component.scss']
+})
+export class BtmComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
